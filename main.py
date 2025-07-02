71908794c07e9c596ae8bd32c3469659e6b03c37
charaf1995 <charafeddineaitelmouden@gmail.com>
2025-07-02 13:09:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,9 +34,37 @@ def metadata(table_name: str):
   <edmx:DataServices>
     <Schema xmlns="http://docs.oasis-open.org/odata/ns/edm" Namespace="{table_name}Model">
       <EntityType Name="{table_name}">
-        <Key><PropertyRef Name="id"/></Key>
-        <Property Name="id" Type="Edm.Int32" Nullable="false"/>
-        <!-- Add more fields if you want -->
+        <Key><PropertyRef Name="Year"/></Key>
+        <Property Name="Year" Type="Edm.Int32" Nullable="false"/>
+        <Property Name="Month" Type="Edm.Int32"/>
+        <Property Name="DayofMonth" Type="Edm.Int32"/>
+        <Property Name="DayOfWeek" Type="Edm.Int32"/>
+        <Property Name="DepTime" Type="Edm.String"/>
+        <Property Name="CRSDepTime" Type="Edm.String"/>
+        <Property Name="DepDelay" Type="Edm.String"/>
+        <Property Name="ArrTime" Type="Edm.String"/>
+        <Property Name="CRSArrTime" Type="Edm.String"/>
+        <Property Name="ArrDelay" Type="Edm.String"/>
+        <Property Name="UniqueCarrier" Type="Edm.String"/>
+        <Property Name="FlightNum" Type="Edm.String"/>
+        <Property Name="TailNum" Type="Edm.String"/>
+        <Property Name="ActualElapsedTime" Type="Edm.String"/>
+        <Property Name="CRSElapsedTime" Type="Edm.String"/>
+        <Property Name="AirTime" Type="Edm.String"/>
+        <Property Name="Origin" Type="Edm.String"/>
+        <Property Name="Dest" Type="Edm.String"/>
+        <Property Name="Distance" Type="Edm.Double"/>
+        <Property Name="TaxiIn" Type="Edm.String"/>
+        <Property Name="TaxiOut" Type="Edm.String"/>
+        <Property Name="Cancelled" Type="Edm.String"/>
+        <Property Name="CancellationCode" Type="Edm.String"/>
+        <Property Name="Diverted" Type="Edm.String"/>
+        <Property Name="CarrierDelay" Type="Edm.String"/>
+        <Property Name="WeatherDelay" Type="Edm.String"/>
+        <Property Name="NASDelay" Type="Edm.String"/>
+        <Property Name="SecurityDelay" Type="Edm.String"/>
+        <Property Name="LateAircraftDelay" Type="Edm.String"/>
+        <Property Name="Column1" Type="Edm.String"/>
       </EntityType>
       <EntityContainer Name="Container">
         <EntitySet Name="{table_name}" EntityType="{table_name}Model.{table_name}"/>
@@ -45,7 +73,6 @@ def metadata(table_name: str):
   </edmx:DataServices>
 </edmx:Edmx>'''
     return Response(content=metadata_xml.strip(), media_type="application/xml")
-
 
 # ✅ Main proxy endpoint
 @app.get("/odata/{table_name}")
