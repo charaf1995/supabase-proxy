fb3a7a8eff852b3fe0eb0040b5e6a42a33a4973d
charaf1995 <charafeddineaitelmouden@gmail.com>
2025-07-02 02:39:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,31 +24,55 @@ SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")  # Set this in Render environme
 def root():
     return {"status": "Supabase proxy is running"}
 
-# ✅ OData metadata endpoint (needed for SAC to show fields)
-@app.get("/odata/{table_name}/$metadata")
+# ✅ @app.get("/odata/{table_name}/$metadata")
 def metadata(table_name: str):
-    # Replace the fields below with your actual Supabase table columns and types
-    metadata_xml = f"""<?xml version="1.0" encoding="utf-8"?>
+    metadata_xml = """<?xml version="1.0" encoding="UTF-8"?>
 <edmx:Edmx xmlns:edmx="http://docs.oasis-open.org/odata/ns/edmx" Version="4.0">
   <edmx:DataServices>
-    <Schema xmlns="http://docs.oasis-open.org/odata/ns/edm" Namespace="{table_name}_schema">
-      <EntityType Name="{table_name}">
+    <Schema xmlns="http://docs.oasis-open.org/odata/ns/edm" Namespace="flights_schema">
+      <EntityType Name="flights">
         <Key>
-          <PropertyRef Name="id"/>
+          <PropertyRef Name="Year"/>
         </Key>
-        <Property Name="id" Type="Edm.Int32"/>
-        <Property Name="departure" Type="Edm.String"/>
-        <Property Name="arrival" Type="Edm.String"/>
-        <Property Name="delay" Type="Edm.Int32"/>
-        <!-- ➕ Add more <Property> elements here if your table has more columns -->
+        <Property Name="Year" Type="Edm.Double"/>
+        <Property Name="Month" Type="Edm.Double"/>
+        <Property Name="DayofMonth" Type="Edm.Double"/>
+        <Property Name="DayOfWeek" Type="Edm.Double"/>
+        <Property Name="DepTime" Type="Edm.Double"/>
+        <Property Name="CRSDepTime" Type="Edm.Double"/>
+        <Property Name="DepDelay" Type="Edm.String"/>
+        <Property Name="ArrTime" Type="Edm.Double"/>
+        <Property Name="CRSArrTime" Type="Edm.Double"/>
+        <Property Name="ArrDelay" Type="Edm.Double"/>
+        <Property Name="UniqueCarrier" Type="Edm.String"/>
+        <Property Name="FlightNum" Type="Edm.Double"/>
+        <Property Name="TailNum" Type="Edm.String"/>
+        <Property Name="ActualElapsedTime" Type="Edm.Double"/>
+        <Property Name="CRSElapsedTime" Type="Edm.Double"/>
+        <Property Name="AirTime" Type="Edm.Double"/>
+        <Property Name="Origin" Type="Edm.String"/>
+        <Property Name="Dest" Type="Edm.String"/>
+        <Property Name="Distance" Type="Edm.Double"/>
+        <Property Name="TaxiIn" Type="Edm.Double"/>
+        <Property Name="TaxiOut" Type="Edm.Double"/>
+        <Property Name="Cancelled" Type="Edm.String"/>
+        <Property Name="CancellationCode" Type="Edm.Double"/>
+        <Property Name="Diverted" Type="Edm.Double"/>
+        <Property Name="CarrierDelay" Type="Edm.Double"/>
+        <Property Name="WeatherDelay" Type="Edm.Double"/>
+        <Property Name="NASDelay" Type="Edm.Double"/>
+        <Property Name="SecurityDelay" Type="Edm.Double"/>
+        <Property Name="LateAircraftDelay" Type="Edm.Double"/>
+        <Property Name="Column1" Type="Edm.String"/>
       </EntityType>
-      <EntityContainer Name="{table_name}Container">
-        <EntitySet Name="{table_name}" EntityType="{table_name}_schema.{table_name}"/>
+      <EntityContainer Name="flightsContainer">
+        <EntitySet Name="flights" EntityType="flights_schema.flights"/>
       </EntityContainer>
     </Schema>
   </edmx:DataServices>
 </edmx:Edmx>"""
     return Response(content=metadata_xml, media_type="application/xml")
+
 
 # ✅ Main proxy endpoint (data from Supabase to SAC)
 @app.get("/odata/{table_name}")
